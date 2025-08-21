ce8d2535d2a8b6dba8f61914da0466e3d2369c65
Nikolai Afanasyev <mininik08inv@yandex.ru>
2025-08-21 16:01:30 +0300

    е..ля

--- website_about_novaya_lyada/settings/base.py
+++ website_about_novaya_lyada/settings/base.py
@@ -7,6 +7,9 @@ load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
+# Добавьте путь к apps в PYTHONPATH
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.join(BASE_DIR, 'apps'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
